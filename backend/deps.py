246f81843189c7a467ba43211ad3ce8b64458642
Dylan <dylanjtoner@gmail.com>
2025-06-03 20:53:09 +0100

    fix redis connection

--- backend/deps.py
+++ backend/deps.py
@@ -1,10 +1,12 @@
 import redis.asyncio as redis
+import os
 from functools import lru_cache
 
 
 @lru_cache
 def get_redis_client() -> redis.Redis:
-    return redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+    redis_host = os.getenv("REDIS_HOST", "localhost")
+    return redis.Redis(host=redis_host, port=6379, db=0, decode_responses=True)
 
 
 async def get_redis() -> redis.Redis:
