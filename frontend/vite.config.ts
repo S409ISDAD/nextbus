78676ced2a887a08809b84ea3a872ca97d62a4f2
Dylan <dylanjtoner@gmail.com>
2025-06-10 09:36:05 +0100

    add ts host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    allowedHosts: ["dylan-mint-laptop.tailefc815.ts.net"]
+  }
 
 })
