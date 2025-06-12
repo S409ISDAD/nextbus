75d4b9de0c8e75899851203776f838c69b763c05
Dylan <dylanjtoner@gmail.com>
2025-06-12 19:52:49 +0100

    remove unused

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ git pull origin main
 echo "stopping containers..."
 docker compose down -v
 
+echo "removing unused Docker resources..."
+docker system prune -f
+
 echo "rebuilding and starting containers..."
 docker compose up --build -d
 
