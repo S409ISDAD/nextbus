71545cee38d53e58a9d0fdc34c1902e970398cce
Dylan <dylanjtoner@gmail.com>
2025-06-19 21:37:45 +0100

    update corrections

--- backend/utils/time.py
+++ backend/utils/time.py
@@ -5,12 +5,12 @@ from datetime import timedelta
 def check_scheduled_time(scheduled: dt, current_time: dt) -> dt:
     time_difference = (scheduled - current_time).total_seconds()
 
-    # time is more than 6h in the future so bus is most likely yesterday, subtract 1 day
-    if time_difference / 3600 > 6:
+    # time is more than 12h in the future so bus is most likely yesterday, subtract 1 day
+    if time_difference / 3600 > 12:
         scheduled -= timedelta(days=1)
 
-    # time is more than 6h in the past so bus is most likely tomorrow, add 1 day
-    elif time_difference / 3600 < -6:
+    # time is more than 12h in the past so bus is most likely tomorrow, add 1 day
+    elif time_difference / 3600 < -12:
         scheduled += timedelta(days=1)
 
     return scheduled
