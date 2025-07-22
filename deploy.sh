35588309100cfe9ce16a2671fb0cdc37ec19cf43
Dylan <dylanjtoner@gmail.com>
2025-07-22 21:50:11 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ echo "stopping containers..."
 docker compose stop
 
 echo "rebuilding and starting containers..."
-docker compose -f docker-compose.fly.yml up --build -d
+docker compose -f docker-compose.prod.yml up --build -d
 
 echo "deployment complete."
