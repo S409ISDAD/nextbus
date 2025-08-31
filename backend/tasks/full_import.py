9a2267641c96bd19d15203fd89f6227a9ea0f10e
Dylan Toner <dylanjtoner@gmail.com>
2025-08-31 15:04:29 +0100

    make data available

--- backend/tasks/full_import.py
+++ backend/tasks/full_import.py
@@ -4,11 +4,11 @@ from backend.tasks.import_naptan import import_naptan_data
 
 
 # import_naptan_data("NaPTAN.xml")
-import_naptan_data("190.xml")
+import_naptan_data("static_data/190.xml")
 print("✔ NAPTAN data imported successfully")
-txc_importer = TXCImporter("64_txc.xml")
+txc_importer = TXCImporter("static_data/64_txc.xml")
 txc_importer.handle_txc_file()
-txc_importer = TXCImporter("67_txc.xml")
+txc_importer = TXCImporter("static_data/67_txc.xml")
 txc_importer.handle_txc_file()
 # import_txc_zip("scso_all.zip")
 print("✔ TXC data imported successfully")
