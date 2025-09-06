f3ccd8a016dd7c64cbd24eeec9ca62e9c168eed8
Dylan Toner <dylanjtoner@gmail.com>
2025-09-06 15:28:56 +0100

    remove old sw (temp)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,4 +5,13 @@ import "leaflet/dist/leaflet.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from "./App.tsx";
 
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const registration of registrations) {
+            console.log("Unregistering old service worker", registration);
+            registration.unregister();
+        }
+    });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
