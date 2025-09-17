61b8cf9b0d55bf019b97d034be570392527b8710
Dylan Toner <dylanjtoner@gmail.com>
2025-09-17 21:41:55 +0100

    fix test

--- backend/tests/test_tracking_confidence.py
+++ backend/tests/test_tracking_confidence.py
@@ -1,17 +1,19 @@
 from geopy import distance
+from shapely.geometry.linestring import LineString
+
 from backend.services.tracking_confidence import track_location_similarity
 
 def test_track_location_similarity():
-    track = [[51.5074, -0.1278], [51.5075, -0.1279]]
-    locations = [[51.5074, -0.1278], [51.5075, -0.1279]]
+    track = LineString([[51.5074, -0.1278], [51.5075, -0.1279]])
+    locations = LineString([[51.5074, -0.1278], [51.5075, -0.1279]])
 
     result = track_location_similarity(track, locations)
 
     assert isinstance(result, float)
     assert result == 1.0
 
-    track = [[51.5075, -0.1278], [51.5075, -0.1279]]
-    locations = [[21.5074, -0.1278], [51.5072, -5.279]]
+    track = LineString([[51.5075, -0.1278], [51.5075, -0.1279]])
+    locations = LineString([[21.5074, -0.1278], [51.5072, -5.279]])
 
     result = track_location_similarity(track, locations)
 
