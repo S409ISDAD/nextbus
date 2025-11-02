556e3c10e49fe87618cb51a2c694b92a61852175
Dylan Toner <dylanjtoner@gmail.com>
2025-11-02 23:54:48 +0000

    capacitor init

---
+++ frontend/android/app/src/main/java/dev/orbitix/nextbus/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.orbitix.nextbus;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.orbitix.nextbus',
+  appName: 'nextbus',
+  webDir: 'dist'
+};
+
+export default config;
