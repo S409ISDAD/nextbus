48448acefdb84726baa99f17ccae6d01a0837090
Dylan Toner <dylanjtoner@gmail.com>
2025-11-03 09:24:00 +0000

    Revert "capacitor init"
    This reverts commit 556e3c10e49fe87618cb51a2c694b92a61852175.

--- frontend/android/app/src/main/java/dev/orbitix/nextbus/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package dev.orbitix.nextbus;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'dev.orbitix.nextbus',
-  appName: 'nextbus',
-  webDir: 'dist'
-};
-
-export default config;
