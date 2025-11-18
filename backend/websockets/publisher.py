dc3abb42e48b396686c85efcd25f34c4feb01ec5
Dylan Toner <dylanjtoner@gmail.com>
2025-11-18 21:10:21 +0000

    fix dupes i hope

--- backend/websockets/publisher.py
+++ backend/websockets/publisher.py
@@ -3,7 +3,6 @@ import json
 from fastapi.encoders import jsonable_encoder
 from time import time
 from redis.asyncio import Redis
-from typing import Set
 from datetime import datetime, timezone
 from ..tasks.get_departures import get_departures
 from backend.deps import DateTimeEncoder, get_redis
@@ -15,6 +14,8 @@ from backend.deps import get_logger
 log = get_logger(__name__)
 
 sync_redis = get_redis(sync=True)
+
+local_publish_tasks: dict[str, asyncio.Task] = {}
 
 REDIS_LOCK_EXPIRE = 60  # seconds
 
@@ -73,23 +74,32 @@ async def publish_loop(channel: str, key: str, redis: Redis):
         await release_publish_lock(redis, channel, key)
         await redis.srem(redis_active_key(channel), key)
         log.debug(f"Publish loop ended for {channel}:{key}")
+        local_publish_tasks.pop(key, None)
 
 
 async def start_publishing(channel: str, key: str, redis: Redis):
+    got_lock = await acquire_publish_lock(redis, channel, key)
+    if not got_lock:
+        log.debug(f"Publish task already active in another process for {key}")
+        return
+
     await redis.sadd(redis_active_key(channel), key)
     log.debug(f"Starting publish task for {key}")
 
-    got_lock = await acquire_publish_lock(redis, channel, key)
-    if not got_lock:
-        log.debug(f"Publishing already active for {key}")
-        return
+    if key not in local_publish_tasks:
+        task = asyncio.create_task(publish_loop(channel, key, redis))
+        local_publish_tasks[key] = task
 
-    asyncio.create_task(publish_loop(channel, key, redis))
+        def _done_callback(t: asyncio.Task):
+            local_publish_tasks.pop(key, None)
+            log.debug(f"Local task removed for {key}")
+
+        task.add_done_callback(_done_callback)
 
 
 async def stop_publishing(channel: str, key: str, redis: Redis):
-    active_set = redis_active_key(channel)
-    await redis.srem(active_set, key)
-
-    await release_publish_lock(redis, channel, key)
-    log.debug(f"Signaled publish loop to stop for {channel}:{key}")
+    await redis.srem(redis_active_key(channel), key)
+    task = local_publish_tasks.pop(key, None)
+    if task:
+        task.cancel()
+        log.debug(f"Cancelled local publish task for {key}")
