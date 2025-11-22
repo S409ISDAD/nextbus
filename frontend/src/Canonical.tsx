03f0c63594b8762b28c250d068e40affdf7b45d8
Dylan Toner <dylanjtoner@gmail.com>
2025-11-22 15:17:23 +0000

    exclude testing

--- frontend/src/Canonical.tsx
+++ frontend/src/Canonical.tsx
@@ -4,12 +4,20 @@ import { useLocation } from "react-router";
 const Canonical = () => {
     const location = useLocation();
 
-    const preferredDomain = "nextbus.org.uk";
+    const currentHost = window.location.host;
 
+    if (
+        currentHost === "staging.nextbus.cc" ||
+        currentHost === "nextbus2.orbitix.dev"
+    ) {
+        return null;
+    }
+
+    const preferredDomain = "nextbus.org.uk";
     const canonicalUrl = `https://${preferredDomain}${location.pathname}${location.search}`;
 
     return (
-        <Helmet>
+        <Helmet key="canonical">
             <link rel="canonical" href={canonicalUrl} />
         </Helmet>
     );
